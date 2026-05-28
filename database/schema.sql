f5de856b6236108d71676afa34463a611491e948
myganglody <mingang1022@naver.com>
2026-05-28 17:12:14 +0900

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -37,7 +37,7 @@ CREATE TABLE clubs (
   clubId BIGINT PRIMARY KEY AUTO_INCREMENT,
 
   clubName VARCHAR(100) NOT NULL,
-  briefDescription VARCHAR(30),
+  briefDescription VARCHAR(90),
   description TEXT,
   activity TEXT,
 
