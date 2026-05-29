7070c87867f9e58c2709fc57d2ad1739e40f8704
DaAn <andh0812@naver.com>
2026-05-30 05:44:48 +0900

    [chore] Express MySQL 연결 구조 개선

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
@@ -10,21 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log('DB 연결 실패');
-    console.log(err);
-  } else {
-    console.log('DB 연결 성공!');
-  }
-});
+(async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log('DB 연결 성공!');
+        connection.release();
+    } catch (err) {
+        console.log('DB 연결 실패');
+        console.log(err);
+    }
+})();
 
 app.get('/', (req, res) => {
   res.send('서버 실행 중!');
