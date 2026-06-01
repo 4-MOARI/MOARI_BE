5e3d7f3e9388b8ffce7bf6fc50035ed8faeabad6
이정현 <ijeonghyeon@ijeonghyeon-ui-MacBookPro.local>
2026-06-01 16:42:54 +0900

    feat: auth API structure

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express');
 const cors = require('cors');
 
 const reviewRoutes = require('./routes/reviewRoutes');
-const reportRoutes =
-    require('./routes/reportRoutes');
-//검색 API 추가
+const reportRoutes = require('./routes/reportRoutes');
+// 검색 API 추가
 const clubRoutes = require('./routes/clubRoutes');
-const favoriteRoutes =
-    require('./routes/favoriteRoutes');
-const userRoutes =
-    require('./routes/userRoutes');
+const favoriteRoutes = require('./routes/favoriteRoutes');
+const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
 const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -21,10 +20,11 @@ app.get('/', (req, res) => {
   res.send('서버 실행 중!');
 });
 
+app.use('/auth', authRoutes);
 
 app.use(reviewRoutes);
 app.use('/api', reportRoutes);
-//검색 API 추가
+// 검색 API 추가
 app.use('/api', clubRoutes);
 app.use('/api', favoriteRoutes);
 app.use('/api', userRoutes);
